64040ed1df49e50fbbbd35e0b58518d8f3961f8f
luthor112 <luthor112@gmail.com>
2022-08-17 19:45:22 +0200

    Handle all monitors

--- main.cpp
+++ main.cpp
@@ -2,28 +2,48 @@
 #include <iostream>
 #include <physicalmonitorenumerationapi.h>
 #include <highlevelmonitorconfigurationapi.h>
+#include <vector>
+#include <utility>
 
 #pragma comment(lib, "Dxva2")
 
 #define RELEASE
+//#define SINGLE
 
-int wmain(int argc, wchar_t* argv[])
+BOOL MonitorEnumProc(HMONITOR monitor, HDC, LPRECT, LPARAM allPhysArrays)
 {
-	HWND fgWin = GetForegroundWindow();
-	HMONITOR monitor = MonitorFromWindow(fgWin, MONITOR_DEFAULTTONEAREST);
 	DWORD physLen;
 	if (!GetNumberOfPhysicalMonitorsFromHMONITOR(monitor, &physLen))
 	{
 		std::wcout << L"Error querying monitors" << std::endl;
-		return 1;
 	}
 	LPPHYSICAL_MONITOR physArray = (LPPHYSICAL_MONITOR)malloc(physLen * sizeof(PHYSICAL_MONITOR));
 	if (!GetPhysicalMonitorsFromHMONITOR(monitor, physLen, physArray))
 	{
 		std::wcout << L"Error querying physical monitors" << std::endl;
-		return 1;
 	}
 
+	((std::vector<std::pair<DWORD, LPPHYSICAL_MONITOR>>*)allPhysArrays)->push_back(std::pair<DWORD, LPPHYSICAL_MONITOR>(physLen, physArray));
+
+	return TRUE;
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+	std::vector<std::pair<DWORD, LPPHYSICAL_MONITOR>> allPhysArrays;
+
+#ifdef SINGLE
+	HWND fgWin = GetForegroundWindow();
+	HMONITOR monitor = MonitorFromWindow(fgWin, MONITOR_DEFAULTTONEAREST);
+	MonitorEnumProc(monitor, NULL, NULL, (LPARAM)&allPhysArrays);
+#else
+	if (!EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM)&allPhysArrays))
+	{
+		std::wcout << L"Error enumerating monitors" << std::endl;
+		return 1;
+	}
+#endif
+	
 	if (RegisterHotKey(NULL, 1, MOD_ALT | MOD_NOREPEAT, VK_F9) &&
 		RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, VK_F10))
 	{
@@ -49,26 +69,38 @@ int wmain(int argc, wchar_t* argv[])
 		{
 			std::wcout << L"WM_HOTKEY received" << std::endl;
 
-			DWORD minBr, currBr, maxBr;
-			GetMonitorBrightness(physArray[0].hPhysicalMonitor, &minBr, &currBr, &maxBr);
-			std::wcout << physArray[0].szPhysicalMonitorDescription << " (#0 of " << physLen << ") has brightness " << currBr << std::endl;
-
-			switch (msg.wParam)
+			for (auto& current : allPhysArrays)
 			{
-			case 1:
-				SetMonitorBrightness(physArray[0].hPhysicalMonitor, max(minBr, currBr - 1));
-				break;
-			case 2:
-				SetMonitorBrightness(physArray[0].hPhysicalMonitor, min(currBr + 1, maxBr));
-				break;
-			}
+				DWORD physLen = current.first;
+				LPPHYSICAL_MONITOR physArray = current.second;
 
-			std::wcout << L"Brightness modified" << std::endl;
+				for (int i = 0; i < physLen; ++i)
+				{
+					DWORD minBr, currBr, maxBr;
+					GetMonitorBrightness(physArray[i].hPhysicalMonitor, &minBr, &currBr, &maxBr);
+					std::wcout << physArray[i].szPhysicalMonitorDescription << " has brightness " << currBr << std::endl;
+
+					switch (msg.wParam)
+					{
+					case 1:
+						SetMonitorBrightness(physArray[i].hPhysicalMonitor, max(minBr, currBr - 1));
+						break;
+					case 2:
+						SetMonitorBrightness(physArray[i].hPhysicalMonitor, min(currBr + 1, maxBr));
+						break;
+					}
+
+					std::wcout << L"Brightness modified" << std::endl;
+				}
+			}
 		}
 	}
 
-	DestroyPhysicalMonitors(physLen, physArray);
-	free(physArray);
+	for (auto& current : allPhysArrays)
+	{
+		DestroyPhysicalMonitors(current.first, current.second);
+		free(current.second);
+	}
 
 	return 0;
 }
